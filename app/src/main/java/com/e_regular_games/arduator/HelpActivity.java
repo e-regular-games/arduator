5a7b6ecaa1ac7717588783916a86c676969d88a1
Ryan Edgar <dayoldryebread@gmail.com>
2017-10-01 09:25:01 -0400

    feat(help): improve help and add email button

--- app/src/main/java/com/e_regular_games/arduator/HelpActivity.java
+++ app/src/main/java/com/e_regular_games/arduator/HelpActivity.java
@@ -1,6 +1,8 @@
 package com.e_regular_games.arduator;
 
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class HelpActivity extends AppCompatActivity {
@@ -24,6 +27,18 @@ public class HelpActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        Button email = (Button) findViewById(R.id.help_button_email);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"e.regular.games.llc@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Arduator: Setup Question?");
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
